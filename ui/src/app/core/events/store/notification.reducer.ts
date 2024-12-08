98c9ec219338aa3d89bd488ed5fde849acc22f5b
developerForSale <developerforsale@outlook.com>
2024-12-08 13:06:09 +0800

    Next: presents group data to users, using mat-tree I guess?

--- ui/src/app/core/events/store/notification.reducer.ts
+++ ui/src/app/core/events/store/notification.reducer.ts
@@ -3,7 +3,6 @@ import { cloneDeep, create } from 'lodash';
 import { createReducer, on } from '@ngrx/store';
 import { SSEActions } from './notification.actions';
 
-
 /**
  * Data format of events receive from SSE.
  */
@@ -15,7 +14,7 @@ type RSVeEvent = {
   RSVePhase: string;
   // Id of the group this event belongs to.
   group: string;
-}
+};
 
 export const errorLevel = '40';
 
@@ -23,8 +22,7 @@ export type EventType = {
   event: any;
   level: string;
   id: string;
-}
-
+};
 
 /**
  * Data structure of RSV executor events store.
@@ -38,8 +36,7 @@ export type RSVePhaseNode = {
   // Id list of events tagged for this phase,
   // It also sorts the events in order of time (earliest to latest)
   events: string[] | null;
-}
-
+};
 
 /**
  * NgRx store data structure.
@@ -47,7 +44,7 @@ export type RSVePhaseNode = {
 export type EventRecord = {
   event: any;
   level: string;
-}
+};
 
 export interface NotificationState {
   isSubscribed: boolean;
@@ -61,7 +58,7 @@ export interface NotificationState {
     // RSVe events organized by group, using groupId as key
     RSVeEventGroups: Record<string, RSVePhaseNode>;
     // For firstly received RSVe event of each group, mapping groupId to its index in idList
-    RSVeEventGroupIndexMap: Record<number, string>;
+    RSVeEventGroupIndexMap: Record<string, number>;
   };
 }
 
@@ -81,30 +78,10 @@ export const notificationReducer = createReducer(
   initialState,
   on(SSEActions.eventReceived, (state, { event }) => {
     if (isRSVeEvent(event.event)) {
-      const isFirstEventOfNewGroup: boolean = !state.events.RSVeEventGroups.hasOwnProperty(event.event.group)
       return {
         ...state,
-        events: {
-          ...state.events,
-          // If it's first event of a new group, added its eventId to idList
-          idList: isFirstEventOfNewGroup ? 
-            [...state.events.idList, event.id] : state.events.idList,
-          eventRecords: {
-            ...state.events.eventRecords,
-            [event.id]: {
-              event: event.event,
-              level: event.level,
-            },
-          },
-          RSVeEventGroups: addNewRSVeEvent(state.events.RSVeEventGroups, event),
-          // If it's first event of a new group, added its groupId to RSVeEventGroupIndexMap
-          RSVeEventGroupIndexMap: isFirstEventOfNewGroup ?
-            {
-              ...state.events.RSVeEventGroupIndexMap,
-              [state.events.idList.length]: event.event.group
-            } : state.events.RSVeEventGroupIndexMap
-        },
-      }
+        events: addNewRSVeEvent(state.events, event),
+      };
     } else {
       return {
         ...state,
@@ -118,8 +95,8 @@ export const notificationReducer = createReducer(
               level: event.level,
             },
           },
-        }
-      }
+        },
+      };
     }
   }),
   on(SSEActions.errorAlarm, (state, { error }) => ({
@@ -147,18 +124,107 @@ function isRSVeEvent(event: string | RSVeEvent): event is RSVeEvent {
   return typeof event === 'object' && 'RSVePhase' in event;
 }
 
+function addNewRSVeEvent(
+  events: NotificationState['events'],
+  event: EventType
+): NotificationState['events'] {
+  const isFirstEventOfNewGroup: boolean =
+    !events.RSVeEventGroups.hasOwnProperty(event.event.group);
+  const isProgressEvent: boolean = event.event.type === 'progress';
+  var oldProgressEventId: string | null = null;
+
+  // First we find the right node of the group to add the event
+  var group: RSVePhaseNode;
+  if (events.RSVeEventGroups.hasOwnProperty(event.event.group)) {
+    // If the group already exists, find the right node to add the event.
+    var group = cloneDeep(events.RSVeEventGroups[event.event.group]);
+    var node = findOrCreateTheNode(group, event.event.RSVePhase);
+    if (node.events === null) node.events = [event.id];
+    else {
+      if (isProgressEvent) {
+        // If new one is a progress event, Check if there is old one or not.
+        for (let eventId of node.events) {
+          if (events.eventRecords[eventId].event.type === 'progress') {
+            oldProgressEventId = eventId;
+            break;
+          }
+        }
+        if (oldProgressEventId !== null) {
+          // If there is old one, replace it with the new one.
+          node.events = node.events.map((id) => {
+            if (id === oldProgressEventId) {
+              return event.id;
+            } else {
+              return id;
+            }
+          });
+        }
+      }
+      if (oldProgressEventId === null) node.events.push(event.id);
+    }
+  } else {
+    // Create a new group if the group does not exist.
+    group = {
+      phase: event.event.RSVePhase,
+      nextPhase: null,
+      subPhase: null,
+      events: [event.id],
+    };
+  }
+
+  // Then, update idList, eventRecords, and RSVeEventGroupIndexMap
+  // If it's first event of a new group, added its eventId to idList
+  var idList = isFirstEventOfNewGroup
+    ? [...events.idList, event.id]
+    : events.idList;
+  var records = {
+    ...events.eventRecords,
+    [event.id]: { event: event.event, level: event.level },
+  };
+  // If it's first event of a new group, added its groupId to RSVeEventGroupIndexMap
+  var indexMap = isFirstEventOfNewGroup
+    ? {
+        ...events.RSVeEventGroupIndexMap,
+        [event.event.group]: events.idList.length,
+      }
+    : events.RSVeEventGroupIndexMap;
+  if (oldProgressEventId !== null) {
+    if (oldProgressEventId === idList[indexMap[event.event.group]]) {
+      idList[indexMap[event.event.group]] = event.id;
+    }
+    delete records[oldProgressEventId];
+  }
+
+  return {
+    ...events,
+    idList: idList,
+    eventRecords: records,
+    RSVeEventGroups: {
+      ...events.RSVeEventGroups,
+      [event.event.group]: group,
+    },
+    RSVeEventGroupIndexMap: indexMap,
+  };
+}
+
 function phaseToNumberList(phase: string): number[] {
   return phase.split('.').map((s) => parseInt(s));
 }
 
-function findOrCreateTheNode(node: RSVePhaseNode, phase: string): RSVePhaseNode {
-  var currentNode = node
-  const targetPhase = phaseToNumberList(phase)
+function findOrCreateTheNode(
+  node: RSVePhaseNode,
+  phase: string
+): RSVePhaseNode {
+  var currentNode = node;
+  const targetPhase = phaseToNumberList(phase);
   // phaseLevel indicates the comparison index between targetPhase and phaseToNumberList(currentNode.phase).
-  var phaseLevel = 0
+  var phaseLevel = 0;
 
   while (phaseLevel < targetPhase.length) {
-    if (targetPhase[phaseLevel] === phaseToNumberList(currentNode.phase)[phaseLevel]) {
+    if (
+      targetPhase[phaseLevel] ===
+      phaseToNumberList(currentNode.phase)[phaseLevel]
+    ) {
       if (phaseLevel === targetPhase.length - 1) {
         break;
       } else {
@@ -167,11 +233,11 @@ function findOrCreateTheNode(node: RSVePhaseNode, phase: string): RSVePhaseNode 
             phase: targetPhase.slice(0, phaseLevel + 2).join('.'),
             nextPhase: null,
             subPhase: null,
-            events: null
-          }
+            events: null,
+          };
         }
-        currentNode = currentNode.subPhase
-        phaseLevel++
+        currentNode = currentNode.subPhase;
+        phaseLevel++;
       }
     } else {
       if (currentNode.nextPhase === null) {
@@ -179,45 +245,12 @@ function findOrCreateTheNode(node: RSVePhaseNode, phase: string): RSVePhaseNode 
           phase: targetPhase.slice(0, phaseLevel + 1).join('.'),
           nextPhase: null,
           subPhase: null,
-          events: null
-        }
+          events: null,
+        };
       }
-      currentNode = currentNode.nextPhase
+      currentNode = currentNode.nextPhase;
     }
   }
 
-  return currentNode
-}
-
-/**
- * Add a new RSVe event to RSVeEventGroups.
- * @param eventGroups current RSVeEventGroups state
- * @param event new RSVe event from SSE
- */
-function addNewRSVeEvent(
-  eventGroups: Record<string, RSVePhaseNode>,
-  event: EventType,
-): Record<number, RSVePhaseNode> {
-  if (isRSVeEvent(event.event)) {
-    var group: RSVePhaseNode   
-    if (eventGroups.hasOwnProperty(event.event.group)) {
-      // If the group already exists, find the right node to add the event.
-      var group = cloneDeep(eventGroups[event.event.group]);
-      var node = findOrCreateTheNode(group, event.event.RSVePhase);
-      node.events = node.events === null ? [event.id] : [...node.events, event.id];
-    } else {
-      // Create a new group if the group does not exist.
-      group = {
-        phase: event.event.RSVePhase,
-        nextPhase: null,
-        subPhase: null,
-        events: [event.id]
-      }
-    }
-    return {
-      ...eventGroups,
-      [event.event.group]: group
-    }
-  }
-  return eventGroups
+  return currentNode;
 }
